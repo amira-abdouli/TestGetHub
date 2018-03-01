2111ba72a63090dae6ce74c90a63b00fb736d0e5
Amira Abdouli <a.abdouli@root.sa>
2018-03-01 12:16:16 +0100

    Add project files.

---
+++ TestGetHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestGetHub.Startup))]
+namespace TestGetHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
